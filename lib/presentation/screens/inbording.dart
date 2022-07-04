5fa8b80a1873979d6d139bf336190d7aebb84833
aml <amlmusa1357@gmail.com>
2022-07-04 22:22:20 +0200

    add

--- lib/presentation/screens/inbording.dart
+++ lib/presentation/screens/inbording.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import '../widgets/inbording_screen_model.dart';
+import '../widgets/onBording_data.dart';
 import '../widgets/rectangle_indicator.dart';
 
 class InBording extends StatefulWidget {
@@ -13,37 +12,17 @@ class InBording extends StatefulWidget {
 class _InBordingState extends State<InBording> {
   int currentIndex = 0;
   final controller = PageController();
-  List <Data>myData = [
-    Data(
-      imageUrl: 'assets/images/onboarding1',
-      title:
-          'Getting to the gym and prioritizing your fitness isn’t easy—we know! ',
-      subtitle:
-          'But staying on track and working toward your health goals is essential',
-    ),
-    Data(
-      imageUrl: 'assets/images/onboarding2',
-      title: 'All progress takes place outside the comfort zone.',
-      subtitle:
-          'Bodybuilding is much like any other sport. To be successful, you must dedicate yourself 100% to your training, diet and mental approach.',
-    ),
-    Data(
-      imageUrl: 'assets/images/onboarding3',
-      title: 'Put all excuses aside and remember this: You are capable.',
-      subtitle:
-          'If we could give every individual the right amount of nourishment and exercise, not too little and not too much, we would have found the safest way to health.',
-    ),
-  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(15),
-          child: Stack(
+          child: Column(
             children: [
               Align(
-                alignment: Alignment.topRight,
+                alignment: AlignmentDirectional.topEnd,
                 child: ElevatedButton(
                   onPressed: () {},
                   child: Text(
@@ -57,98 +36,99 @@ class _InBordingState extends State<InBording> {
                       )),
                 ),
               ),
-              Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text(
-                          '7',
-                          style: TextStyle(
-                              color: Colors.amber,
-                              fontSize: 50,
-                              fontWeight: FontWeight.bold),
+              RichText(
+                text: TextSpan(
+                    text: '7',
+                    style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'krave',
+                        style: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 57,
+                          fontFamily: 'Arista Pro',
                         ),
-                        Text(
-                          'krave',
-                          style: TextStyle(
-                            color: Colors.teal,
-                            fontSize: 57,
-                            fontFamily: 'Arista Pro',
+                      )
+                    ]),
+              ),
+              SizedBox(height: 20,),
+              Expanded(
+                child: PageView.builder(
+                  itemCount: 3,
+                  itemBuilder: (BuildContext context, int index) {
+                    return SingleChildScrollView(
+                      child: Column(
+                        children: [
+                         Image.asset(
+                              myData[index].imageUrl,
+                              fit: BoxFit.cover,
+                           width: 300,
+                           height: 200,
+                            ),
+                          SizedBox(height: 20,),
+                          Text(
+                            myData[index].title,
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
-                          //textAlign: TextAlign.center,
-                        )
-                      ],
-                    ),
-                    Container(
-                      height: 400,
-                      child: PageView.builder(
-                          itemCount: 3,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Column(
-                              children: [
-                                Image.asset(
-                                      width: 250,
-                                      height: 250,
-                                        myData[index].imageUrl,
-                                      fit: BoxFit.contain,
-                                    ),
-                                Text(
-                                  myData[index].title,
-                                  style: TextStyle(
-                                    fontSize: 30,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  textAlign: TextAlign.center,
-                                ),
-                                Text(
-                                  myData[index].subtitle,
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.black45
-                                  ),
-                                  textAlign: TextAlign.center,
-                                )
-                              ],
-                            );
-                          },
-                      onPageChanged: (index){
-                            setState(() {
-                              currentIndex=index;
-                            });
-                      },),
-                    ),
-                    Center(
-                      child: RectangleIndicator(index: currentIndex),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Get Started',
-                        style: TextStyle(color: Colors.black),
+                          SizedBox(height: 20,),
+                          Text(
+                            myData[index].subtitle,
+                            style:
+                                TextStyle(fontSize: 20, color: Colors.black45),
+                            textAlign: TextAlign.center,
+                          )
+                        ],
                       ),
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.all(20),
-                          primary: Colors.teal,
-                          fixedSize: Size(double.infinity, 30),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          )),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("Don/'t have an account?"),
-                        TextButton(onPressed: (){},
-                            child: Text('Sign up',style: TextStyle(color: Colors.teal),))
-
-                      ],
-                    ),
-                  ],
+                    );
+                  },
+                  onPageChanged: (index) {
+                    setState(() {
+                      currentIndex = index;
+                    });
+                  },
                 ),
               ),
+              SizedBox(height: 20,),
+              Center(
+                child: RectangleIndicator(index: currentIndex),
+              ),
+              SizedBox(height: 20,),
+              ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Get Started',
+                    style: TextStyle(color: Colors.white,
+                    fontSize: 20),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      //padding: EdgeInsets.all(20),
+                      primary: Colors.teal,
+                      minimumSize: Size(double.infinity, 60),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      )),
+                ),
+              SizedBox(height: 20,),
+              RichText(
+                  text: TextSpan(
+                      text: "Don/'t have an account? ",
+                      style: TextStyle(color: Colors.black,
+                      fontSize: 20),
+                      children: [
+                    TextSpan(
+                      text: 'Sign up',
+                      style: TextStyle(color: Colors.teal
+                      ,fontSize: 20),
+                    )
+                  ])),
             ],
           ),
         ),
---
+++ lib/presentation/widgets/onBording_data.dart
@@ -0,0 +1,23 @@
+import 'inbording_screen_model.dart';
+
+List<Data> myData = [
+  Data(
+    imageUrl: 'assets/images/onbording1.png',
+    title:
+    'Getting to the gym and prioritizing your fitness isn’t easy—we know! ',
+    subtitle:
+    'But staying on track and working toward your health goals is essential',
+  ),
+  Data(
+    imageUrl: 'assets/images/onbording2.png',
+    title: 'All progress takes place outside the comfort zone.',
+    subtitle:
+    'Bodybuilding is much like any other sport. To be successful, you must dedicate yourself 100% to your training, diet and mental approach.',
+  ),
+  Data(
+    imageUrl: 'assets/images/onbording3.png',
+    title: 'Put all excuses aside and remember this: You are capable.',
+    subtitle:
+    'If we could give every individual the right amount of nourishment and exercise, not too little and not too much, we would have found the safest way to health.',
+  ),
+];
